291d160d78f6ae18370cb46fd117012956f80363
mrblack360 <rmaswi360@outlook.com>
2020-09-08 00:25:19 +0300

    Modified edit methods

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,13 +4,19 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { HomeComponent } from './home.component';
+import { EditDialogComponent } from '../edit-dialog/edit-dialog.component';
 import { FormsModule } from '@angular/forms';
 
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
+import {
+  MatDialogModule,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, EditDialogComponent],
   imports: [
     CommonModule,
     SharedModule,
@@ -18,6 +24,12 @@ import { MatIconModule } from '@angular/material/icon';
     FormsModule,
     MatMenuModule,
     MatIconModule,
+    MatDialogModule,
   ],
+  providers: [
+    { provide: MatDialogRef, useValue: {} },
+    { provide: MAT_DIALOG_DATA, useValue: [] },
+  ],
+  entryComponents: [EditDialogComponent],
 })
 export class HomeModule {}
