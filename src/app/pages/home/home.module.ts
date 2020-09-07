afe331ac7a59350eea2d98e74409af8a0dffb50e
mrblack360 <rmaswi360@outlook.com>
2020-09-07 11:18:22 +0300

    modified the delete and edit methods

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -6,8 +6,18 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { HomeComponent } from './home.component';
 import { FormsModule } from '@angular/forms';
 
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, SharedModule, HomeRoutingModule, FormsModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    HomeRoutingModule,
+    FormsModule,
+    MatMenuModule,
+    MatIconModule,
+  ],
 })
 export class HomeModule {}
